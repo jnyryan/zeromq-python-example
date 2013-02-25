45db6d63ffc4dcfa6df4066f2ec17504906984ff
glgapp <glgapp@ubuntu.(none)>
2013-02-25 15:25:54 +0000

    initial commit

---
+++ zmq_listener.py
@@ -0,0 +1,9 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+socket.bind("tcp://127.0.0.1:5000")
+
+while True:
+  msg = socket.recv()
+  print "Received %s" % msg
---
+++ zmq_publisher.py
@@ -0,0 +1,6 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.PUSH)
+socket.connect('tcp://127.0.0.1:5000')
+socket.send("Hello World!")
